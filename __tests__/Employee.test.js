671d4a8d88baff76a8d763e82ce864261e1a2981
Kelly Wacker <kelwacker@gmail.com>
2022-09-16 00:11:55 -0400

    employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,42 @@
 const Employee = require('../lib/Employee.js');
+const id = 17;
+const name = 'Kelly';
+const email = 'kelwacker@gmail.com';
+
+// create new employee / blank
+test("create new employee", () => {
+    const employee = new Employee();
+    expect(typeof(employee)).toBe("object");
+});
+
+// test create object / info
+test('create new employee with info', () => {
+    const employee = new Employee(id, name, email);
+    
+    expect(employee.id).toBe(id);
+    expect(employee.name).toBe(name);
+    expect(employee.email).toBe(email);
+});
+
+// get employee id
+test('get employee id', () => {
+    const employee = new Employee(id, name, email);
+    expect(employee.getId()).toBe(id);
+});
+
+// get employee name
+test('get employee name', () => {
+    const employee = new Employee(id, name, email);
+    expect(employee.getName()).toBe(name);
+});
+
+// get employee email
+test('get employee email', () => {
+    const employee = new Employee(id, name, email);
+    expect(employee.getEmail()).toBe(email);
+});
+
+test('get employee name', () => {
+    const employee = new Employee(id, name, email);
+    expect(employee.getRole()).toBe('Employee');
+});
